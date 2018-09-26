ed7d2f82a726126bdf42c4d85a3d5431071cfefa
njacob1001 <njacob1001@gmail.com>
2018-09-25 19:57:09 -0500

    impruve node server

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const cors = require('cors');
+// const cors = require('cors');
+let state = 1;
 
 
  process.env.PORT = process.env.PORT || 3000;
-app.use(cors());
+// app.use(cors());
 app.use(express.urlencoded({ extended:false }));
 app.use(express.json());
 
-let state = 1;
-
 app.use(express.static( path.resolve( __dirname, './public' )));
 
- app.post('/state/:estado', (req,res) => {
+app.post('/change/:estado', (req,res) => {
      let newState = req.params.estado;
      state = Number(newState);
 
      res.send(`Estado cambiado ${newState}`)
- });
+});
 
- app.get('/state', (req,res) => {
-     res.set('Content-Type', 'application/json');
-     res.write(JSON.stringify({state:state}));
- });
+app.get('/state', (req,res) => {
+     console.log('entro');
+     res.header('Content-Type', 'application/json');
+     res.json({ok: true, state});
+     console.log('salio xd');
+});
 
  app.get('/restart', (req,res) => {
      state = 1;
-     res.set('Content-Type', 'application/json')
-     res.write(JSON.stringify({mensaje: 'Estado reincioado, estado actual: 1'}));
+     res.header('Content-Type', 'application/json')
+     res.json({ mensaje: 'Estado reincioado, estado actual: 1' });
  });
 
  
